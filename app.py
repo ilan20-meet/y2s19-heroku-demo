dbd4487bfdcd78d3bb0b6355f9f685d19f333815
myname21-meet <myname21@meet.mit.edu>
2019-07-31 10:45:23 -0400

    afwa

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_tempate, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_tempate("cool.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
